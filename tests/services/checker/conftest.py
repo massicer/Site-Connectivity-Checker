bc9631ef67c3c6422c96a050f02e5a6c44e5e669
Massimiliano Ceriani <massimilianoceriani08@gmail.com>
2020-06-13 02:22:10 +0200

    fix: controller perform request test suite

--- tests/services/checker/conftest.py
+++ tests/services/checker/conftest.py
@@ -1,17 +1,15 @@
 import asyncio
 
 import pytest
+import asynctest
 
 from site_connectivity_checker.services.checker import Checker
 
 
 @pytest.fixture
 async def mocked_request_performer(mocker):
-    async def return_async_value(url):
-        return mocker.MagicMock(id="return async value")
-
     req_performer = mocker.MagicMock(id="async request performer")
-    req_performer.perform_request = return_async_value
+    req_performer.perform_request = asynctest.CoroutineMock()
     return req_performer
 
 
